aa3fc1ee761a95046729009c340574db55f666a6
Karjhan <alex_mar1999@yahoo.com>
2023-09-12 12:39:34 +0300

    [alex] added Simulated Annealing class with search function

--- ai-search-algo/local.py
+++ ai-search-algo/local.py
@@ -1,4 +1,6 @@
 from graph import Graph, Node
+import random
+import math
 
 class Hill_Climbing:
     def __init__(self, graph, currentNode, heuristic):
@@ -34,4 +36,46 @@ class Hill_Climbing:
                 else:
                     return (self.CurrentNode, self.Path)
         return (self.CurrentNode, self.Path)
+
+class Simulated_Annealing:
+    def __init__(self, graph, currentNode, initialTemp, coolingRate):
+        self.Graph = graph
+        self.CurrentNode = currentNode
+        self.BestNode = currentNode
+        self.BestCost = currentNode.Cost
+        self.Temperature = initialTemp
+        self.CoolingRate = coolingRate
+        self.Path = []
+    def ChangeGraph(self, newGraph, newCurrentNode):
+        self.Graph = newGraph
+        self.CurrentNode = newCurrentNode
+        self.Path = []
+
+    def ChangeTemperature(self, newTemp):
+        self.Temperature = newTemp
+
+    def ChangeCoolingRate(self, newCoolingRate):
+        self.CoolingRate = newCoolingRate
+
+    def _accept_neighbour(self, neighbour):
+        if neighbour.Cost < self.CurrentNode.Cost:
+            return True
+        probability = math.exp((self.CurrentNode.Cost - neighbour.Cost) / self.temperature)
+        return random.random() < probability
+
+    def AnnealSearch(self, maxIterations):
+        for i in range(maxIterations):
+            self.Path.append(self.CurrentNode)
+            neighbours = self.CurrentNode.Neighbours
+            if len(neighbours) == 0:
+                return (None, [])
+            nextNeighbour = random.choice(neighbours)
+            if self._accept_neighbour(nextNeighbour):
+                self.CurrentNode = nextNeighbour
+                if self.CurrentNode.Cost < self.BestCost:
+                    self.BestNode = self.CurrentNode
+                    self.BestCost = self.CurrentNode.Cost
+            self.Temperature *= self.CoolingRate
+        return (self.BestNode, self.Path)
+
 
