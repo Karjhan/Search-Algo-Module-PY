700f0a04fe08d211ebd06ffd9e9699458ff0d705
Karjhan <alex_mar1999@yahoo.com>
2023-09-12 11:36:20 +0300

    [alex] updated Hill Climbing class to include path in search result

--- ai-search-algo/local.py
+++ ai-search-algo/local.py
@@ -5,16 +5,19 @@ class Hill_Climbing:
         self.Graph = graph
         self.CurrentNode = currentNode
         self.Heuristic = heuristic
+        self.Path = []
 
     def ChangeGraph(self, newGraph, newCurrentNode):
         self.Graph = newGraph
         self.CurrentNode = newCurrentNode
+        self.Path = []
 
     def Search(self, searchValue, maxIterations = 1000, ascend = False):
         for i in range(maxIterations):
+            self.Path.append(self.CurrentNode)
             neighbours = self.CurrentNode.Neighbours
             if len(neighbours) == 0:
-                return None
+                return (None,[])
             currentHeurValue = self.Heuristic(self.CurrentNode, searchValue)
             if ascend:
                 bestNeighbour = max(neighbours, key = lambda x: self.Heuristic(x,searchValue))
@@ -22,13 +25,13 @@ class Hill_Climbing:
                 if bestHeurValue > currentHeurValue:
                     self.CurrentNode = bestNeighbour
                 else:
-                    return self.CurrentNode
+                    return (self.CurrentNode, self.Path)
             else:
                 bestNeighbour = min(neighbours, key = lambda x: self.Heuristic(x, searchValue))
                 bestHeurValue = self.Heuristic(bestNeighbour, searchValue)
                 if bestHeurValue < currentHeurValue:
                     self.CurrentNode = bestNeighbour
                 else:
-                    return self.CurrentNode
-        return self.CurrentNode
+                    return (self.CurrentNode, self.Path)
+        return (self.CurrentNode, self.Path)
 
